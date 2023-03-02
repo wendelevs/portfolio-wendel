7ced4a17d71eb723b18522d5424de01ff3debf65
Wendel Evangelista <107012134+wendelevs@users.noreply.github.com>
2023-03-02 19:42:40 -0300

    atualização conteúdo.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* =============== Animação Escrevendo ===============*/
+
+var typed = new Typed(".typing", {
+    strings:["Desenvolvedor Web"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
