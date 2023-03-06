475b37e6a0a29839f7a6d682b3c251315e572b93
Wendel Evangelista <107012134+wendelevs@users.noreply.github.com>
2023-03-06 17:54:25 -0300

    atualização conteúdo.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ var typed = new Typed(".typing", {
 
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length;
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
     for(let i=0; i<totalNavList; i++) {
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function(){
@@ -23,6 +25,9 @@ const nav = document.querySelector(".nav"),
         })
     }
     function showSection(element) {
+        for(let i=0; i<totalSection; i++) {
+            allSection[i].classList.remove("active");
+        }
         const target = element.getAttribute("href").split("#")[1];
         document.querySelector("#" + target).classList.add("active")
     }
