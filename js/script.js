71eeae8267fe954d3d14362840a6bf70b3146d59
Wendel Evangelista <107012134+wendelevs@users.noreply.github.com>
2023-03-06 18:16:54 -0300

    atualização conteúdo.

--- js/script.js
+++ js/script.js
@@ -46,4 +46,7 @@ const nav = document.querySelector(".nav"),
         function asideSectionTogglerBtn() {
             aside.classList.toggle("open");
             navTogglerBtn.classList.toggle("open");
+            for(let i=0; i<totalSection; i++) {
+                allSection[i].classList.toggle("open");
+            }
         }
